de98d821bd1b500cd066faa54d437801feb6193d
dgonza45 <69641287+dgonza45@users.noreply.github.com>
2023-08-30 13:19:34 -0500

    Add files via upload

--- reto2/server2/src/server2.py
+++ reto2/server2/src/server2.py
@@ -1,5 +1,7 @@
 import pika
 import os
+import ssl
+import smtplib
 
 
 def format_message(body):   #Decodifica el binario a string
@@ -8,16 +10,29 @@ def format_message(body):   #Decodifica el binario a string
 def listarqueue(ch, method, properties, body):
     folder_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'serverFiles')
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-    print(files)    #Imprime resultado
+    enviar_correo(files)
     return 
 
 def encontrarqueue(ch, method, properties, body):
     search_files = format_message(body)
     folder_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'serverFiles')
     files_found = [f for f in os.listdir(folder_path) if f.lower() in search_files and os.path.isfile(os.path.join(folder_path, f))]
-    print(files_found)  #Imprime resultado
+    enviar_correo(files_found)
     return 
 
+
+
+def enviar_correo(response):
+        context = ssl.create_default_context()
+        subject = 'Respuesta MOM'
+        message = 'Error en el servicio gRPC, se tramito la solicitud via MOM, la  respuesta de la consulta es\n' + response
+        message = 'Subject: {}\n\n{}'.format(subject, message)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls(context=context)
+        server.login('observatorioproyecto1@gmail.com', os.environ['passcorreo'])
+        server.sendmail('observatorioproyecto1@gmail.com', 'dgonza45@eafit.edu.co', message)
+        server.quit()
+        return
 
 # Escucha
 connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['rabbithost'], 5672,'/', pika.PlainCredentials(os.environ['user'], os.environ['password'])))
